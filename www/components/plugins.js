e665c3bd96921d0f4af4772bd914fb29fd52c4e2
guhsilvs <augustin.gomes1512@gmail.com>
2024-11-01 07:32:08 +0900

    Project's Initial Commit

---
+++ www/components/plugins.js
@@ -0,0 +1,16 @@
+// This is a JavaScript file
+
+$(document).on("click, #camera",function(){
+
+navigator.camera.getPicture(onSuccess, onFail, { quality: 50, destinationType: Camera.DestinationType.FILE_URI });
+
+function onSuccess(imageURI) {
+    var image = document.getElementById('myImage');
+    image.src = imageURI;
+}
+
+function onFail (message) {
+    alert('Failed because: ' + message);
+
+}
+});
